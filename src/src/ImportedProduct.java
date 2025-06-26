66056b4ef907322268dcf9ba0b37341ca697a5a5
EnzoCalligaris <enzocontatobr@gmail.com>
2025-06-26 13:45:47 -0300

    Projeto fibnalizado

--- src/src/ImportedProduct.java
+++ src/src/ImportedProduct.java
@@ -1,2 +1,32 @@
-public class ImportedProduct {
+
+public class ImportedProduct extends Product{
+    private Double customsFee;
+
+    public ImportedProduct(String name, Double price, Double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public Double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(Double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    @Override
+    public String priceTag() {
+        return getName()
+                + " $ "
+                + String.format("%.2f", totalPrice())
+                + "(Customs fee: $ "
+                + String.format("%.2f", customsFee)
+                + ")";
+    }
+
+    public Double totalPrice(){
+        return super.getPrice() + 20.00;
+    }
+
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.print("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)?");
+            System.out.print("Name:");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Price: ");
+            double price = sc.nextDouble();
+            char ch = sc.next().charAt(0);
+            if(ch == 'c'){
+                list.add(new Product(name, price));
+            }
+            else if (ch == 'u'){
+                System.out.println("Manufacture date (DD/MM/YYYY): ");
+                LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                list.add(new UsedProduct(name, price, date));
+            }
+            else {
+                System.out.print("Taxa de importacao: ");
+                double customsFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price, customsFee));
+            }
+
+            System.out.println();
+            System.out.println("PRICE TAGS:");
+            for (Product prod : list){
+                System.out.println(prod.priceTag());
+            }
+
+            sc.close();
         }
     }
 }
